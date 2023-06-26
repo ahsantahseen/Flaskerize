8ae25ba922da088a67632474932cb94d72e2f94b
ahsantahseen <ahsan.tntboy@gmail.com>
2023-06-26 11:40:20 +0000

    Added Json Body Validation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask,request,jsonify,make_response
+from flask import Flask, request, jsonify, make_response
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
 import pymysql
@@ -9,14 +9,15 @@ import uuid
 import jwt
 import datetime
 
-from schema.student import student_schema,students_schema
-from schema.user import user_schema,users_schema
+from schema.student import student_schema, students_schema, validation_schema_students
+from schema.user import user_schema, users_schema
 from models.student import Student
 from models.user import User
 from db.db import db
+from utils import validate_json
 
 
-#Student API
+# Student API
 
 # 1. Basic CRUD Done
 # 2. Needs Authentication using JWT
@@ -24,8 +25,8 @@ from db.db import db
 # 4. Docker Deployment with Nginx
 
 
-app=Flask(__name__) 
-limiter=Limiter(
+app = Flask(__name__)
+limiter = Limiter(
     get_remote_address,
     app=app,
     default_limits=['30 per hour'],
@@ -34,110 +35,124 @@ limiter=Limiter(
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ahsan:ahsan1234@localhost:3306/student_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
-app.config['SECRET_KEY']='ahsan'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['SECRET_KEY'] = 'ahsan'
 
 db.init_app(app)
 
+
 def token_required(f):
-   @wraps(f)
-   def decorator(*args, **kwargs):
-       token = None
-       if 'x-access-token' in request.headers:
-           token = request.headers['x-access-token']
- 
-       if not token:
-           return jsonify({'message': 'a valid token is missing'})
-       try:
-           data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-           current_user = User.query.filter_by(id=data['id']).first()
-       except:
-           return jsonify({'message': 'token is invalid'})
- 
-       return f(current_user, *args, **kwargs)
-   return decorator
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+        try:
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = User.query.filter_by(id=data['id']).first()
+        except:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(current_user, *args, **kwargs)
+    return decorator
+
 
 with app.app_context():
     db.create_all()
 
-@app.route("/students",methods=['GET'])
+
+@app.route("/students", methods=['GET'])
 @token_required
 def get_students(current_user):
-    students=Student.query.all()
-    result=students_schema.dump(students)
+    students = Student.query.all()
+    result = students_schema.dump(students)
     return jsonify(result)
 
-@app.route("/student/<id>",methods=['GET'])
+
+@app.route("/student/<id>", methods=['GET'])
 @token_required
-def get_student(current_user,id):
-    student=db.session.get(Student,int(id))
+def get_student(current_user, id):
+    student = db.session.get(Student, int(id))
     return student_schema.jsonify(student)
 
-@app.route('/student',methods=['POST'])
+
+@app.route('/student', methods=['POST'])
 @token_required
 def add_student(current_user):
-    name=request.json['name']
-    cgpa=request.json['cgpa']
-    age=request.json['age']
-    semester=request.json['semester']
+    if validate_json(validation_schema_students, request.json):
+        name = request.json['name']
+        cgpa = request.json['cgpa']
+        age = request.json['age']
+        semester = request.json['semester']
 
-    new_student=Student(name,cgpa,age,semester)
-    db.session.add(new_student)
-    db.session.commit()
-    return student_schema.jsonify(new_student)
-
-@app.route("/student/<id>",methods=['PUT'])
-@token_required
-def update_student(current_user,id):
-    student=db.session.get(Student,id)
-    if student:
-         name=request.json['name']
-         cgpa=request.json['cgpa']
-         age=request.json['age']
-         semester=request.json['semester']
-
-         student.name=name
-         student.cgpa=cgpa
-         student.age=age
-         student.semester=semester
-         db.session.commit()
-         return student_schema.jsonify(student)
+        new_student = Student(name, cgpa, age, semester)
+        db.session.add(new_student)
+        db.session.commit()
+        return student_schema.jsonify(new_student)
     else:
-        return make_response(jsonify({"Error":'Student Not Found'}),404)
+        return make_response(jsonify({"Error": 'Incomplete/Incorrect Data, Please Try Again!'}), 400)
 
 
-   
-
-@app.route("/student/<id>",methods=['DELETE'])
+@app.route("/student/<id>", methods=['PUT'])
 @token_required
-def delete_student(current_user,id):
-    student=db.session.get(Student,id)    
+def update_student(current_user, id):
+    student = db.session.get(Student, id)
+    if student:
+        if validate_json(validation_schema_students, request.json):
+            name = request.json['name']
+            cgpa = request.json['cgpa']
+            age = request.json['age']
+            semester = request.json['semester']
+
+            student.name = name
+            student.cgpa = cgpa
+            student.age = age
+            student.semester = semester
+            db.session.commit()
+            return student_schema.jsonify(student)
+        else:
+            return make_response(jsonify({"Error": 'Incomplete/Incorrect Data, Please Try Again!'}), 400)
+    else:
+        return make_response(jsonify({"Error": 'Student Not Found'}), 404)
+
+
+@app.route("/student/<id>", methods=['DELETE'])
+@token_required
+def delete_student(current_user, id):
+    student = db.session.get(Student, id)
     if student:
         db.session.delete(student)
         db.session.commit()
         return student_schema.jsonify(student)
     else:
-        return make_response(jsonify({"Error":'Student Not Found'}),404)    
-    
+        return make_response(jsonify({"Error": 'Student Not Found'}), 404)
 
-@app.route("/register",methods=['POST'])
+
+@app.route("/register", methods=['POST'])
 def register_user():
-    name=request.json['name']
-    email=request.json['email']
-    password=generate_password_hash(request.json['password'],method='sha256')
-    new_user=User(name,email,password)
+    name = request.json['name']
+    email = request.json['email']
+    password = generate_password_hash(
+        request.json['password'], method='sha256')
+    new_user = User(name, email, password)
     db.session.add(new_user)
     db.session.commit()
     return user_schema.jsonify(new_user)
 
-@app.route("/login",methods=['POST'])
+
+@app.route("/login", methods=['POST'])
 def login_user():
-    user=User.query.filter_by(email=request.json['email']).first()
-    if check_password_hash(user.password,request.json['password']):
-        token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=45)}, app.config['SECRET_KEY'], "HS256")
-        return jsonify({"token":token})
-    return make_response(jsonify({"Error":'Login Required'}),401)
+    user = User.query.filter_by(email=request.json['email']).first()
+    if check_password_hash(user.password, request.json['password']):
+        token = jwt.encode({'id': user.id, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(minutes=45)}, app.config['SECRET_KEY'], "HS256")
+        return jsonify({"token": token})
+    return make_response(jsonify({"Error": 'Login Required'}), 401)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
--- schema/student.py
+++ schema/student.py
@@ -1,12 +1,35 @@
 from flask_marshmallow import Marshmallow
 
-marshmallow=Marshmallow()
+marshmallow = Marshmallow()
+
+validation_schema_students = {
+    "name": {
+        "type": "string",
+        "maxlength": 80,
+        "required": True
+    },
+    "age": {
+        "type": "integer",
+        "min": 18,
+        "required": True
+    },
+    "cgpa": {
+        "type": "float",
+        "required": True
+    },
+    "semester": {
+        "type": "integer",
+        "min": 1,
+        "required": True
+    },
+}
+
 
 class StudentSchema(marshmallow.Schema):
-    #The meta data we want to output in our JSON Response
+    # The meta data we want to output in our JSON Response
     class Meta:
-        fields=["id","name","semester","cgpa","age"]
+        fields = ["id", "name", "semester", "cgpa", "age"]
+
 
 student_schema = StudentSchema()
 students_schema = StudentSchema(many=True)
-
